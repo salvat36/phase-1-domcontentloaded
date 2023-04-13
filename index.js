3ecfd1caaeac8499b8d3e36451c274df7c855807
Ryan <salvat36@gmail.com>
2023-04-12 21:01:38 -0500

    Lab completed although, not sure why console log doesnt show the string The Dom has Loaded

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The Dom has loaded");
+    let textReplace = document.getElementById("text");
+    textReplace.innerHTML = "This is really cool!"
+});
+
